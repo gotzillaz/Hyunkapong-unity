af22e8ad85c6c6443cb9b83847f80c63da73654d
Gotzillaz <got.bodin@gmail.com>
2014-07-17 13:14:37 +0700

    Try to translate GameObject in code

--- Assets/GameScreenGUI.js
+++ Assets/GameScreenGUI.js
@@ -18,10 +18,10 @@ function OnGUI()
 	var resizeRatio : float = bgRatio/screenRatio;
 	var resizeRatio2 : float = logoRatio/screenRatio;
 	//Debug.Log(resizeRatio+" "+bgRatio+" "+screenRatio);
-	GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),background);
+//	GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),background);
 	//GUI.Box(new Rect(Screen.width/2.0-Screen.width*resizeRatio2/2,0,Screen.width*resizeRatio2,Screen.height*resizeRatio2),logoPic,GUIStyle.none);
-	GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),screenTop);
-	GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),screenBottom);
+//	GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),screenTop);
+//	GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),screenBottom);
 	stageLabel.fontSize = Screen.height*0.1;
 	stageLabel.padding = new RectOffset(0,0,Screen.height*0.025,0);
 	GUI.Label(new Rect(0,0,Screen.width,Screen.height),"Stage " + StageInfo.stageNO,stageLabel);
---
+++ Assets/PlateGen.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+var testTexture : Texture;
+var a : GameObject;
+var b : GameObject;
+function Start () {
+	a = GameObject.Find("PlateGen");
+	b = GameObject.Find("PlateGen2");
+	//Graphics.DrawTexture(Rect(Screen.height/2.0-100,Screen.width/2.0-100,200,200),testTexture);
+	//Debug.Log("XYZZZZ");
+}
+
+function Update () {/*
+	var pa = a.transform.position[1];
+	var pb = b.transform.position[1];
+	if(pa>pb){
+		a.transform.Translate(0,-2,0);
+		b.transform.Translate(0,2,0);
+	}
+	else{
+		a.transform.Translate(0,2,0);
+		b.transform.Translate(0,-2,0);
+	}*/
+}
+
+function OnGUI () {
+	if(GUI.Button(Rect(0,0,100,100),"Click"))
+	{
+		OnClick();
+		Debug.Log("XX");
+	}
+}
+
+function OnClick () {
+	var pa = a.transform.position[1];
+	var pb = b.transform.position[1];
+	if(pa>pb){
+		a.transform.Translate(0,-2,0);
+		b.transform.Translate(0,2,0);
+	}
+	else{
+		a.transform.Translate(0,2,0);
+		b.transform.Translate(0,-2,0);
+	}
+}
